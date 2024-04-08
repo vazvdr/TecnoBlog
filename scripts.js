3b5930cd00454f86166ddcd526f522565d84269f
vazvdr <azevedo.vanderson@hotmail.com>
2024-04-07 21:40:33 -0300

    Enviando as imagens e o botao que muda a cor

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function changeStyle_black(){
+    document.getElementById('postagem').className = 'postagem2';
+    document.getElementById('conteudo-lateral').className = 'conteudo-lateral2'
+    document.getElementById('categorias').className = 'conteudo-lateral2'
+    document.getElementById('categorias').className = 'h3-black'
+    document.getElementById('postagens-recentes').className = 'h3-black'
+}
+function changeStyle_white(){
+    document.getElementById('postagem').className = 'postagem';
+    document.getElementById('conteudo-lateral').className = 'conteudo-lateral'
+    document.getElementById('categorias').className = 'conteudo-lateral'
+    document.getElementById('categorias').className = 'h3-white'
+    document.getElementById('postagens-recentes').className = 'h3-white'   
+}
